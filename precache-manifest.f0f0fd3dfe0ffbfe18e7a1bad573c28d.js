807b8bedc4a1c0340ec90afff3c390b50189053a
luisdanielmata93 <ld.dominguez.mata@gmail.com>
2020-03-11 10:09:08 -0600

    Updates

---
+++ precache-manifest.f0f0fd3dfe0ffbfe18e7a1bad573c28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096ffc7511f3649d188c7e782df00541",
+    "url": "/monster-relodex/index.html"
+  },
+  {
+    "revision": "ae03467633f4e7cb5d16",
+    "url": "/monster-relodex/static/css/main.057acc49.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monster-relodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-relodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae03467633f4e7cb5d16",
+    "url": "/monster-relodex/static/js/main.2eb9d3b5.chunk.js"
+  },
+  {
+    "revision": "b26f335dfbf7442b2770",
+    "url": "/monster-relodex/static/js/runtime-main.a74793f9.js"
+  }
+]);
